a0b8d05973d6732e0c6e74e8f3381c609c72df1e
haorh <haorh@mail.ustc.edu.cn>
2023-03-29 20:53:30 +0800

    test

---
+++ src/containers/login/login.jsx
@@ -0,0 +1,14 @@
+/*
+注册路由组件
+ */
+import React, {Component} from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>register</div>
+        );
+    }
+}
+
+export default Register;
